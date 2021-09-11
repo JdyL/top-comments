423175f8f01e0fcd861d325c810ade4bef5c3194
Anon <imma.mulexd@gmail.com>
2021-09-12 00:33:17 +1000

    Fix permission issue and start work

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,49 @@
-import ForgeUI, { render, Fragment, Macro, Text } from "@forge/ui";
+import api, { route } from "@forge/api";
+import ForgeUI, {
+  render,
+  Fragment,
+  Text,
+  Macro,
+  useProductContext,
+  useState,
+  Heading,
+} from "@forge/ui";
+
+const fetchCommentsForContent = async (contentId) => {
+  const res = await api
+    .asUser()
+    .requestConfluence(
+      route`/rest/api/content/${contentId}/child/comment?expand=body.storage,metadata.likes`
+    );
+
+  const data = await res.json();
+  return data.results;
+};
 
 const App = () => {
+  const context = useProductContext();
+  const [comments] = useState(
+    async () => await fetchCommentsForContent(context.contentId)
+  );
+
+  console.log(comments[0].body, comments[0].metadata);
+
   return (
     <Fragment>
+      <Heading>Most liked comment</Heading>
       <Text>Hello world!</Text>
     </Fragment>
   );
 };
 
-export const run = render(
-  <Macro
-    app={<App />}
-  />
-);
+export const run = render(<Macro app={<App />} />);
+
+// editor: '',
+//     atlas_doc_format: '',
+//     view: '',
+//     export_view: '',
+//     styled_view: '',
+//     dynamic: '',
+//     storage: '',
+//     editor2: '',
+//     anonymous_export_view: ''
